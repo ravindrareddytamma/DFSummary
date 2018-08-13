940840f46855447974c7592a3332f346b5314046
TAMMA RAVINDRA REDDY <ravindrareddytamma@gmail.com>
2018-08-13 19:50:28 +0530

    Update DFSummary.R

--- R/DFSummary.R
+++ R/DFSummary.R
@@ -57,18 +57,31 @@ numeric_summary <- function(df)
 #' @return res
 #' @export factor_summary
 
-factor_summary <- function(df)
+factor_summary <- function(df) 
 {
-  fact.char_ind <- which(sapply(df,class) %in% c("character","factor"))
-  fact_df <- subset(df,select = colnames(df)[fact.char_ind])
-  uniq.levels <- sapply(fact_df,function(x){length(levels(as.factor(x)))})
-  mode <- sapply(fact_df,function(x){levels(as.factor(x))[which(table(x) == max(table(x)))]})
-  mode.freq <- sapply(fact_df,function(x){max(table(x))})
-  per.levels <- sapply(fact_df,function(x){
-    round(sum(cumsum(sort(table(x)/sum(table(x))*100,decreasing = T)) < 80)/length(levels(as.factor(x)))*100,3)})
-  res <- data.frame(uniq.levels,mode,mode.freq,per.levels)
-  res <- as.data.frame(t(res),stringsAsFactors = F)
-  res <- tibble::rownames_to_column(res,var = "STATISTIC")
+  fact.char_ind <- which(sapply(df, class) %in% c("character","factor"))
+  fact_df <- subset(df, select = colnames(df)[fact.char_ind])
+  ## Unique Levels
+  uniq.levels <- sapply(fact_df, function(x) {
+    length(levels(as.factor(x)))
+  })
+  
+  ## Mode of the Data Column
+  mode <- sapply(fact_df, function(x) {
+    names(table(x)[which.max(table(x))])
+  })
+  mode.freq <- sapply(fact_df, function(x) {
+    table(x)[which.max(table(x))]
+  })
+  
+  ## Percentage of Most repeating Level
+  per.levels <- sapply(fact_df, function(x) {
+    round(sum(cumsum(sort(table(x)/sum(table(x)) * 100, decreasing = T)) < 
+                80)/length(levels(as.factor(x))) * 100, 3)
+  })
+  res <- data.frame(uniq.levels, mode, mode.freq, per.levels)
+  res <- as.data.frame(t(res), stringsAsFactors = F)
+  res <- tibble::rownames_to_column(res, var = "STATISTIC")
   return(res)
 }
 
